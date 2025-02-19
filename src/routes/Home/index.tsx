303b789df22d1744d1ec2af0f97a9ac0120633d9
sargisshahinyan <shahinyan.sargis@gmail.com>
2025-02-19 23:36:37 +0100

    feat: Create basic masonry view based on fetched images

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,8 +1,43 @@
+import { useMemo } from "react";
+
+import { useGetCuratedPhotos } from "../../api/pexel/getCuratedPhotos/useGetCuratedPhotos.ts";
+
+import { generateImagesMatrix } from "./helpers/generateImagesMatrix.ts";
+
+import { Column, Container, Image, ImageWrapper } from "./styles";
+
 export const Home = () => {
+  const { data } = useGetCuratedPhotos({
+    perPage: 25,
+  });
+
+  const photosColumns = useMemo(() => {
+    if (!data) {
+      return undefined;
+    }
+
+    const photos = data.pages.flatMap((page) => page.photos);
+
+    return generateImagesMatrix(photos);
+  }, [data]);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container>
+      {photosColumns?.map((photos, index) => (
+        <Column key={index}>
+          {photos.map((photo) => (
+            <ImageWrapper key={photo.id}>
+              <Image
+                srcSet={`${photo.src.small} 768w, ${photo.src.medium} 1920w, ${photo.src.original} 2560w`}
+                src={photo.src.medium}
+                alt={photo.photographer}
+                loading="lazy"
+              />
+            </ImageWrapper>
+          ))}
+        </Column>
+      ))}
+    </Container>
   );
 };
 
---
+++ src/routes/Home/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Column = styled.div`
+  flex-basis: 20%;
+`;
+
+export const ImageWrapper = styled.div`
+  padding: 0 0.25rem 0.25rem;
+  position: relative;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: auto;
+`;
