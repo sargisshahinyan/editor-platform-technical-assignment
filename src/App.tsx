f690d6dcb69813158a232a58d9a340667a63eb41
sargisshahinyan <shahinyan.sargis@gmail.com>
2025-02-19 21:57:09 +0100

    feat: Setup react router with the basic home page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { lazy } from "react";
+
+const Home = lazy(() => import("./routes/Home"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -11,7 +15,11 @@ const queryClient = new QueryClient({
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Hello, world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
