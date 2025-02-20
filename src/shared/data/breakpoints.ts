71488cb064d3fd556356a3baa08cbb51a641c8d5
sargisshahinyan <shahinyan.sargis@gmail.com>
2025-02-20 20:16:52 +0100

    feat: Add new breakpoints

--- src/shared/data/breakpoints.ts
+++ src/shared/data/breakpoints.ts
@@ -1,7 +1,8 @@
 export const breakpoints = {
   tablet: 768,
   smallDesktop: 1024,
-  desktop: 1440,
-  largeDesktop: 1920,
-  ["4k"]: 2560,
+  desktop: 1280,
+  largeDesktop: 1440,
+  monitor: 1920,
+  "4k": 2560,
 };
--- src/shared/hooks/useMediaQueries.ts
+++ src/shared/hooks/useMediaQueries.ts
@@ -6,8 +6,9 @@ import { breakpoints } from "../data/breakpoints";
  * - isMobile: boolean, true when the screen is 767px or narrower
  * - isTablet: boolean, true when the screen is 768px or wider
  * - isSmallDesktop: boolean, true when the screen is 1024px or wider
- * - isDesktop: boolean, true when the screen is 1440px or wider
- * - isLargeDesktop: boolean, true when the screen is 1920px or wider
+ * - isDesktop: boolean, true when the screen is 1280px or wider
+ * - isLargeDesktop: boolean, true when the screen is 1440px or wider
+ * - isMonitor: boolean, true when the screen is 1920px or wider
  * - is4k: boolean, true when the screen is 2560px or wider
  */
 export function useMediaQueries() {
@@ -15,6 +16,7 @@ export function useMediaQueries() {
   const isSmallDesktop = useMediaQuery(`(min-width: ${breakpoints.smallDesktop}px)`);
   const isDesktop = useMediaQuery(`(min-width: ${breakpoints.desktop}px)`);
   const isLargeDesktop = useMediaQuery(`(min-width: ${breakpoints.largeDesktop}px)`);
+  const isMonitor = useMediaQuery(`(min-width: ${breakpoints.monitor}px)`);
   const is4k = useMediaQuery(`(min-width: ${breakpoints["4k"]}px)`);
 
   return {
@@ -23,6 +25,7 @@ export function useMediaQueries() {
     isSmallDesktop,
     isDesktop,
     isLargeDesktop,
+    isMonitor,
     is4k,
   };
 }
