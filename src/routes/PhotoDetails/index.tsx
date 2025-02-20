4b4fabb951f9120baf17a2cd0f60cee780febfd6
sargisshahinyan <shahinyan.sargis@gmail.com>
2025-02-21 00:38:20 +0100

    feat: Show details of the selected color

--- src/routes/PhotoDetails/index.tsx
+++ src/routes/PhotoDetails/index.tsx
@@ -1,11 +1,27 @@
 import { useEffect } from "react";
 import { useParams, useNavigate } from "react-router";
+import { Link } from "react-router";
 
 import { useGetPhoto } from "../../api/pexel/getPhoto/useGetPhoto.ts";
 
 import picsartLogo from "../../shared/assets/picsart-logo.svg";
+import leftArrow from "../../shared/assets/left-arrow.svg";
 
-import { Container, LogoImage, LogoLink } from "./styles";
+import {
+  BackButtonIcon,
+  BackButtonWrapper,
+  Container,
+  DetailsBlock,
+  DetailsBlockContent,
+  DetailsBlockTitle,
+  DetailsWrapper,
+  LogoImage,
+  LogoLink,
+  Photo,
+  PhotoColor,
+  PhotoDetailsWrapper,
+  PhotoWrapper,
+} from "./styles";
 
 const PhotoDetails = () => {
   const navigate = useNavigate();
@@ -19,7 +35,7 @@ const PhotoDetails = () => {
     }
   }, [id, navigate, isError]);
 
-  if (!id || photo) {
+  if (!id || !photo) {
     return null;
   }
 
@@ -28,6 +44,43 @@ const PhotoDetails = () => {
       <LogoLink to="/">
         <LogoImage src={picsartLogo} alt="Picsart logo" />
       </LogoLink>
+      <BackButtonWrapper>
+        <Link to="/">
+          <BackButtonIcon src={leftArrow} alt="Back" />
+        </Link>
+      </BackButtonWrapper>
+      <PhotoDetailsWrapper>
+        <PhotoWrapper>
+          <Photo src={photo.src.original} />
+        </PhotoWrapper>
+        <DetailsWrapper>
+          <DetailsBlock>
+            <DetailsBlockTitle>Photographer</DetailsBlockTitle>
+            <DetailsBlockContent>{photo.photographer}</DetailsBlockContent>
+          </DetailsBlock>
+          <DetailsBlock>
+            <DetailsBlockTitle>Photographer URL</DetailsBlockTitle>
+            <DetailsBlockContent>
+              <a href={photo.photographerUrl} target="_blank" rel="noreferrer">
+                {photo.photographerUrl}
+              </a>
+            </DetailsBlockContent>
+          </DetailsBlock>
+          <DetailsBlock>
+            <DetailsBlockTitle>Size</DetailsBlockTitle>
+            <DetailsBlockContent>
+              {photo.width} x {photo.height}
+            </DetailsBlockContent>
+          </DetailsBlock>
+          <DetailsBlock>
+            <DetailsBlockTitle>Photo average color</DetailsBlockTitle>
+            <DetailsBlockContent>
+              <PhotoColor color={photo.avgColor} />
+              {photo.avgColor}
+            </DetailsBlockContent>
+          </DetailsBlock>
+        </DetailsWrapper>
+      </PhotoDetailsWrapper>
     </Container>
   );
 };
--- src/routes/PhotoDetails/styles.ts
+++ src/routes/PhotoDetails/styles.ts
@@ -6,10 +6,10 @@ import { breakpoints } from "../../shared/data/breakpoints";
 import { Link } from "react-router";
 
 export const Container = styled(PageSlot)`
-  padding-top: 2.5rem;
+  padding-block: 2.5rem;
 
   @media (max-width: ${breakpoints.tablet - 1}px) {
-    padding-top: 1rem;
+    padding-block: 1rem;
   }
 `;
 
@@ -17,9 +17,79 @@ export const LogoLink = styled(Link)`
   display: inline-block;
   margin-bottom: 2.25rem;
 
-  @media (max-width: ${breakpoints.tablet}px) {
+  @media (max-width: ${breakpoints.tablet - 1}px) {
     margin-bottom: 1rem;
   }
 `;
 
 export const LogoImage = styled.img``;
+
+export const BackButtonWrapper = styled.div`
+  margin-bottom: 2.25rem;
+
+  @media (max-width: ${breakpoints.tablet - 1}px) {
+    margin-bottom: 1rem;
+  }
+`;
+
+export const BackButtonIcon = styled.img`
+  width: 1rem;
+  height: 0.75rem;
+`;
+
+export const PhotoDetailsWrapper = styled.div`
+  display: flex;
+  gap: 3.75rem;
+
+  @media (max-width: ${breakpoints.tablet - 1}px) {
+    flex-direction: column;
+    gap: 1.5rem;
+  }
+`;
+
+export const PhotoWrapper = styled.div`
+  flex: 2;
+`;
+
+export const Photo = styled.img`
+  border-radius: 1rem;
+  max-width: 100%;
+`;
+
+export const DetailsWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+`;
+
+export const DetailsBlock = styled.div`
+  padding: 0.5rem 1rem;
+  background-color: #d9d9d9;
+  border-radius: 0.625rem;
+`;
+
+export const DetailsBlockTitle = styled.h3`
+  font-size: 1.5rem;
+  font-family: "Gilroy", sans-serif;
+  font-weight: 700;
+  margin-top: 0;
+  margin-bottom: 0.25rem;
+`;
+
+export const DetailsBlockContent = styled.p`
+  font-size: 1rem;
+  font-family: "Gilroy", sans-serif;
+  font-weight: 300;
+  margin: 0;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const PhotoColor = styled.div<{ color: string }>`
+  width: 1.5rem;
+  height: 1.5rem;
+  border-radius: 0.375rem;
+  background-color: ${(props) => props.color};
+`;
