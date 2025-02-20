234f6bf7836106efaf401dd2c3fcfe3620c49939
sargisshahinyan <shahinyan.sargis@gmail.com>
2025-02-20 23:38:31 +0100

    feat: Fetch photo details and navigate back if no data

--- src/api/pexel/getPhoto/useGetPhoto.ts
+++ src/api/pexel/getPhoto/useGetPhoto.ts
@@ -2,9 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 
 import { getPhoto } from "./index";
 
-export const useGetPhoto = (...[photoId]: Parameters<typeof getPhoto>) => {
+export const useGetPhoto = (photoId?: number) => {
   return useQuery({
     queryKey: ["getPhoto", photoId],
-    queryFn: () => getPhoto(photoId),
+    queryFn: photoId ? () => getPhoto(photoId) : undefined,
+    enabled: !!photoId,
   });
 };
--- src/routes/PhotoDetails/index.tsx
+++ src/routes/PhotoDetails/index.tsx
@@ -1,8 +1,28 @@
+import { useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
+
+import { useGetPhoto } from "../../api/pexel/getPhoto/useGetPhoto.ts";
+
 import picsartLogo from "../../shared/assets/picsart-logo.svg";
 
 import { Container, LogoImage, LogoLink } from "./styles";
 
 const PhotoDetails = () => {
+  const navigate = useNavigate();
+  const { id } = useParams<{ id: string }>();
+  const { data: photo, isError } = useGetPhoto(id ? parseInt(id, 10) : undefined);
+
+  useEffect(() => {
+    if (!id || isError) {
+      navigate("/", { replace: true });
+      return;
+    }
+  }, [id, navigate, isError]);
+
+  if (!id || photo) {
+    return null;
+  }
+
   return (
     <Container>
       <LogoLink to="/">
