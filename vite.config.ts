3e771f0bfc8fe73a48d7d4b63e83084ea05e61cf
sargisshahinyan <shahinyan.sargis@gmail.com>
2025-02-22 23:18:32 +0100

    feat: Setup test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
   plugins: [react(), basicSsl()],
+  test: {
+    // 👋 add the line below to add jsdom to vite
+    environment: "jsdom",
+    globals: true,
+  },
 });
